429c4df5866489bf863a7b9b80392a738653ddfd
kellerwald <100421000+kellerwald@users.noreply.github.com>
2023-12-02 21:20:31 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
  var mydata = JSON.parse(data);
- alert(mydata[0].name);
- alert(mydata[1].name);
+// alert(mydata[0].name);
+// alert(mydata[1].name);
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.write(today);
+
+alert(today);
