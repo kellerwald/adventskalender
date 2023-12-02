62be605c8deb8b3c30a019709c80ff6dff073cb5
kellerwald <100421000+kellerwald@users.noreply.github.com>
2023-12-02 21:27:38 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -10,4 +10,21 @@ var yyyy = today.getFullYear();
 today = mm + '/' + dd + '/' + yyyy;
 document.write(today);
 
+
+const compareDates = (d1, d2) => {
+  let date1 = new Date(d1).getTime();
+  let date2 = new Date(d2).getTime();
+
+  if (date1 < date2) {
+    console.log(`Etwas spät, oder?`);
+  } else if (date1 > date2) {
+    console.log(`Du bist zu früh dran.`);
+  } else {
+    console.log(`Bitte sehr`);
+  }
+};
+
+var testDate = "12/01/2023";
+compareDates(today, testDate);
+
 alert(today);
